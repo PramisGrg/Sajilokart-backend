a05885c24dc27833eef5c651d76d55626b8f18c1
PramisGrg <gurungpramis361@gmail.com>
2025-02-01 16:14:30 +0545

    feat: prisma configuration

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import express from "express";
+import { config } from "dotenv";
+config();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const uri = process.env.DATABASE_URI;
+console.log(uri);
 
 app.listen(3000, () => {
   console.log("Server running in port", 3000);
