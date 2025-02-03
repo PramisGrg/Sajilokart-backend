49b81cdff273196cfb60731e30cea8984dab2129
PramisGrg <gurungpramis361@gmail.com>
2025-02-03 23:59:34 +0545

    feat: cloudinary

---
+++ prisma/migrations/20250203091851_user/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "avatar" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
---
+++ src/routes/main.routes.ts
@@ -0,0 +1,3 @@
+import { Router } from "express";
+
+const mainRouter = Router({ mergeParams: true });
